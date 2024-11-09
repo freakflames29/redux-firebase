193a85b578636d2f6d05b18dca99e83c4f3c8ba8
Sourav Das <iamsouravdaswade420@gmail.com>
2024-11-09 13:02:39 +0530

    added firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDhYcdNxFNwQ5OF_nCMYs-4tTO1ZNUFvg",
+  authDomain: "reduxlogin-90a07.firebaseapp.com",
+  projectId: "reduxlogin-90a07",
+  storageBucket: "reduxlogin-90a07.firebasestorage.app",
+  messagingSenderId: "643631933918",
+  appId: "1:643631933918:web:78a8ff5cc62c33a334d54f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
